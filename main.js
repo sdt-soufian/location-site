d8f0a83a4ace2701968104d427e51a20d72da657
sdt-soufian <sdt.soufian@gmail.com>
2021-05-30 17:22:35 +0100

    some changes

--- main.js
+++ main.js
@@ -15,33 +15,16 @@ $(window).on('load', function(){
         var ID = btn.parent().attr('id');
         //console.log($(block[ID]).parent().parent().parent())
         console.log($(Bprincipale[ID]).children().find('.btn-change'))
-        $(Bprincipale[ID]).children().find('.btn-change').on('click', 'button', function(){
-            if($(this).attr('id') == `right${ID}`){
+            if($(Bprincipale[ID]).children().find('.btn-change').children().attr('id') == `right${ID}`){
                 liste[y++].
                 fadeIn().
                 siblings().
                 fadeOut()
             }
             if(y > liste.length-1){
-                y =0;
+                y = 0;
             }
-        })
-        //console.log(ID)
-        /*if(ID == btn.val()){
-            console.log(`I am ${ID}`)
-            if($(block[ID]).btn.attr('id') == 'right'){
-                liste[y++].
-                fadeIn().
-                siblings().
-                fadeOut()
-            }
-            if(y > liste.length-1){
-                y =0;
-            }*/
-        /*}*/
     })
- 
-    
 
 })
 
@@ -49,45 +32,3 @@ $(window).on('load', function(){
 
 
 
-/*
-$(block).each(function(index){
-        var ID = $(this).attr('id');
-        $(this).css("backgroundColor", "red")
-        $(this).on('click', function(){
-            if(index == ID){
-                console.log('true')
-            }
-            else{
-                console.log('false')
-            }
-        })
-    })*/ 
-
-    /*n'a pas marcher
-    
-       $(block).on('click', function(){
-        var ID = $(this).attr('id')
-        console.log(ID)
-        $(block).each(function(index){
-            if(index == ID){
-                console.log($(this))
-                $(this).on('click', 'button', function(){
-                    if($(this).attr('id') == 'right'){
-                        liste[y++]
-                        .fadeIn()
-                        .siblings()
-                        .fadeOut()
-                    }
-                    else if($(this).attr('id') == 'left'){
-                        liste[y--]
-                        .fadeIn()
-                        .siblings()
-                        .fadeOut()
-                    }
-                    if(y > liste.length-1 || y==0){
-                        y = 0
-                    }
-                })
-            }
-        })
-    })*/
